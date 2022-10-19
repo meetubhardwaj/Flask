8f9f84e8334f811f3287cfd28894b51f9f08889e
meetubhardwaj <meetubhardwaj15@gmail.com>
2022-10-19 09:30:46 +0530

    deployment diabetes

--- flask_deployment/main.py
+++ flask_deployment/main.py
@@ -1,9 +1,47 @@
 #pip install flask
-from flask import Flask
+#from crypt import methods
+#from urllib import request
+from flask import Flask,render_template,request
+import joblib
 #initialize the app
 app=Flask(__name__)
+#load the model
+model=joblib.load('diabetes_79.pkl')
 @app.route("/")
 def home():
-    return "hello world"
+    return render_template('main.html')
+@app.route("/contact_us")
+def contact():
+    return render_template('contact_us.html')
+@app.route("/blog")
+def blogs():
+    return render_template('blog.html')
+@app.route("/form",methods=['post'])
+def form():
+    
+    preg=request.form.get('preg')
+    plas=request.form.get('plas')
+    pres=request.form.get('pres')
+    skin=request.form.get('skin')
+    test=request.form.get('test')
+    mass=request.form.get('mass')
+    predi=request.form.get('predi')
+    age=request.form.get('age')
+    
+    
+
+    result=model.predict([[int(preg),int(plas),int(pres),int(skin),int(test),int(mass),int(predi),int(age)]])
+    if result[0]==1:
+        data='person is diabetic'
+    else:
+        data='person is diabetic'
+    return data
+
+
+
+
+
+
+
 #run the app
-app.run() #created a server
+app.run(debug=True) #created a server
